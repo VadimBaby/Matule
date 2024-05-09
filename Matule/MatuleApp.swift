67a9c42f64042bef3d34e99bac12faf9ef198e64
VadimBaby <vadimdim22@gmail.com>
2024-05-09 12:54:05 +0700

    Initial Commit

---
+++ Matule/MatuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatuleApp.swift
+//  Matule
+//
+//  Created by Вадим Мартыненко on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MatuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
